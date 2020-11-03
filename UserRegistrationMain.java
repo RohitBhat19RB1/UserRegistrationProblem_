8a2c7aefcad4d8a9c05ab10e7eab0ac7406999c8
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2020-11-03 10:17:45 +0530

    Welcome to User Registration Project

---
+++ UserRegistrationMain.java
@@ -0,0 +1,7 @@
+public class UserRegistrationMain{
+ public static void main(String[] args) {
+System.out.println("WELCOME TO USER REGISTRATION PROJECT");
+System.out.println("User Registration System needs to ensure all validations are in place during the User Entry");
+System.out.println("Thank you");
+    }
+}
